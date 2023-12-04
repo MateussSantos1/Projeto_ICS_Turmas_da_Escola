b888cb8ba5b959be64fc3779b82c809a2b84aef8
Mateus Santos <115822261+MateussSantos1@users.noreply.github.com>
2023-12-04 10:50:57 -0300

    PROCESSAMENTO_EXCLUSAO_ALUNO_ADM
    Atualizando codigo

--- ifrn-siteicsadm.com/processar_exclusao_alunos.php
+++ ifrn-siteicsadm.com/processar_exclusao_alunos.php
@@ -1,15 +1,5 @@
 <?php
-$servidor = '192.168.100.20';
-$usuario = 'ics';
-$senha = 'conectado';
-$nomebd = 'sistemanotas';
-$conexao = mysqli_connect($servidor, $usuario, $senha, $nomebd);
-if(!$conexao) {
-die("Conexao falhou!!!" + mysqli_connect_error());
-}
-highlight_file('connection.php');
-?>
-<?php
+include('verifica_login.php');
 include('connection.php');
 $aluno = isset($_GET['id']) ? intval($_GET['id']) : 0;
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -17,9 +7,9 @@ $alunoId = intval($_POST["id"]);
 $sql = "DELETE FROM alunos WHERE id = '$alunoId'";
 if($conexao->query($sql) === TRUE){
 echo "Aluno Excluído com sucesso =)";
+header("Location: siteadm.php");
 } else{
 echo "Erro ao excluir o aluno =( " . $conexao->error;
 }
 }
-highlight_file('processar_exclusao_aluno.php');
 ?>
