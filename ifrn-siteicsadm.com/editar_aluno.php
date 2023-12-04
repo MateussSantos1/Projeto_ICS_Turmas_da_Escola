8c0fb563390c18b6a16735d1d3d8d87018753312
Mateus Santos <115822261+MateussSantos1@users.noreply.github.com>
2023-12-04 10:46:44 -0300

    PAGINA_EDICAO_ALUNO_ADM
    Atualizando Codigo

--- ifrn-siteicsadm.com/editar_aluno.php
+++ ifrn-siteicsadm.com/editar_aluno.php
@@ -1,15 +1,5 @@
 <?php
-$servidor = '192.168.100.20';
-$usuario = 'ics';
-$senha = 'conectado';
-$nomebd = 'sistemanotas';
-$conexao = mysqli_connect($servidor, $usuario, $senha, $nomebd);
-if(!$conexao) {
-die("Conexao falhou!!!" + mysqli_connect_error());
-}
-highlight_file('connection.php');
-?>
-<?php
+include('verifica_login.php');
 include('connection.php');
 $alunoId = isset($_GET['id']) ? intval($_GET['id']) : 0;
 if($alunoId> 0){
@@ -24,8 +14,6 @@ die("Aluno não encontrado.");
 else{
 die("ID desse aluno não foi fornecido.");
 }
-$conexao->close();
-highlight_file('editar_aluno.php');
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -37,26 +25,50 @@ highlight_file('editar_aluno.php');
 <div class="container">
 <h2>Editar Aluno</h2>
 <form action="processar_edicao_aluno.php" method="post">
-<input type="hidden" name="id" value=<?php echo $aluno['id']; ?>">
+<input type="hidden" name="id" value="<?php echo $alun
+
+o['id']; ?>">
+
 <label for="nome">Nome:</label>
-<input type="text" name="nome" value="<?php echo $aluno['nome']; ?
->">
+<input type="text" name="nome" value="<?php echo $alun
+
+o['nome']; ?>">
+
 <label for="telefone">Telefone:</label>
-<input type="text" name="telefone" value="<?php echo $alun
-o['telefone']; ?>">
+<input type="text" name="telefone" value="<?php echo $
+
+aluno['telefone']; ?>">
+
+<label for="email">Email</label>
+<input type="email" name="email" value="<?php echo $al
+
+uno['email']; ?>">
+
 <label for="senha">Senha:</label>
-<input type="password" name="senha" value="<?php echo $alun
-o['senha']; ?>">
+<input type="text" name="senha" value="<?php echo $alu
+
+no['senha']; ?>">
+
 <label for="turma_id"> Turma ID:</label>
-<input type="number" name="turma_id" value="<?php echo $alun
-o['turma_id']; ?>">
+<input type="number" name="turma_id" value="<?php echo
+
+$aluno['turma_id']; ?>">
+
 <?php echo"<button><a href='processar_edicao_aluno.php?
+
 id={$aluno['id']}'>Salvar Alterações</a></button>";?>
+
 </form>
-<form action="processar_exclusao_aluno.php" method="post">
-<input type="hidden" name="id" value=<?php echo $aluno['id']; ?
->">
-<button type="submit">Excluir Alunos</button>
+<form action="processar_exclusao_aluno.php" method="po
+
+st">
+
+<input type="hidden" name="id" value=<?php echo $a
+
+luno['id']; ?>">
+
+<button style="background:red;margin-top:1em;" type="submit">Excluir Alunos</button>
+
 </form>
 </div>
 </body>
