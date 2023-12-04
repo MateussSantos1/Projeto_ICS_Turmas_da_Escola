8699739e12d14386f4f546a93fb93506989dac54
Mateus Santos <115822261+MateussSantos1@users.noreply.github.com>
2023-12-04 10:47:45 -0300

    PAGINA_EDICAO_PROFESSOR
    Atualizando codigo

--- ifrn-siteicsadm.com/editar_professor.php
+++ ifrn-siteicsadm.com/editar_professor.php
@@ -1,19 +1,10 @@
 <?php
-$servidor = '192.168.100.20';
-$usuario = 'ics';
-$senha = 'conectado';
-$nomebd = 'sistemanotas';
-$conexao = mysqli_connect($servidor, $usuario, $senha, $nomebd);
-if(!$conexao) {
-die("Conexao falhou!!!" + mysqli_connect_error());
-}
-highlight_file('connection.php');
-?>
-<?php
+include('verifica_login.php');
 include('connection.php');
 $professorId = isset($_GET['id']) ? intval($_GET['id']) : 0;
 if($professorId> 0){
-$sql = "SELECT * FROM professor where professor_id = $professorId";
+$sql = "SELECT * FROM professor where professor_id = $professo
+rId";
 $result = $conexao->query($sql);
 if($result->num_rows > 0){
 $professor = $result->fetch_assoc();
@@ -24,7 +15,7 @@ die("Professor não encontrado.");
 else{
 die("ID desse Professor não foi fornecido.");
 }
-highlight_file('editar_professor.php');
+$conexao->close();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -35,20 +26,49 @@ highlight_file('editar_professor.php');
 <body>
 <div class="container">
 <h2>Editar Professor</h2>
-<form action="processar_edicao_professor.php" method="post">
-<input type="hidden" name="id" value=<?php echo $professo
-r['professor_id']; ?>">
+<form action="processar_edicao_professor.php" method="po
+
+st">
+
+<input type="hidden" name="id" value=<?php echo $profe
+
+ssor['professor_id']; ?>">
+
 <label for="nome_professor">Nome:</label>
-<input type="text" name="nome_professor" value="<?php echo $professo
-r['nome_professor']; ?>">
-<label for="senha_professor">Senha:</label>
-<input type="password" name="senha_professor" value="<?php echo $pro
-fessor['senha_professor']; ?>">
+<input type="text" name="nome_professor" value="<?php
+
+echo $professor['nome_professor']; ?>">
+
 <label for="turma_id"> Turma ID:</label>
-<input type="number" name="turma_id" value="<?php echo $professo
-r['turma_id']; ?>">
-<?php echo"<button><a href='processar_edicao_professor.php?
-id={$professor['professor_id']}'>Salvar Alterações</a></button>";?>
+<input type="number" name="turma_id" value="<?php echo
+
+$professor['turma_id']; ?>">
+<h3>Disciplina</h3>
+<select name="disciplina">
+<option value="Matematica">Matematica</option>
+<option value="Portugues"> Portugues</option>
+<option value="Ciencias"> Ciencias </option>
+<option value="Geografia"> Geografia </option>
+<option value ="Historia"> Historia </option>
+<option value="Ingles"> Ingles </option>
+</select>
+<?php echo"<button><a href='processar_edicao_professor.
+php?id={$professor['professor_id']}'>Salvar Alterações</a></
+button>";?>
+
+</form>
+<form action="processar_exclusao_professor.php" method
+
+="post">
+
+<input type="hidden" name="id" value=<?php echo $p
+
+rofessor['professor_id']; ?>">
+
+<button style="background:red;margin-top:1em;" typ
+
+e="submit">Excluir Professor</button>
+
 </form>
 </div>
 </body>
