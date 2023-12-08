d16bb84428df1c18ad0fa90a76ec0609fb5a2180
Arista22 <130769859+Arista22@users.noreply.github.com>
2023-12-08 14:40:25 -0300

    Update site_cadastro_professor.php

--- ifrn-siteicsadm.com/site_cadastro_professor.php
+++ ifrn-siteicsadm.com/site_cadastro_professor.php
@@ -1,42 +1,41 @@
 <?php
-$servidor = '192.168.100.20';
-$usuario = 'ics';
-$senha = 'conectado';
-$nomebd = 'sistemanotas';
-$conexao = mysqli_connect($servidor, $usuario, $senha, $nomebd
-);
-if(!$conexao) {
-die("Conexao falhou!!!" + mysqli_connect_error());
-}
-highlight_file('connection.php');
-?>
-<?php
+include('verifica_login.php');
 include('connection.php');
 $sql = "select * from professor";
-highlight_file('site_cadastro_professor.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 <meta charset "UTF-8">
 <meta name = "viewport" content="width=device-width, initi
+
 al-scale=1.0">
+
 <link rel="stylesheet" href="style.css">
-<title>Cadastro De Professores </title>
+<title>Cadastro De Alunos </title>
 </head>
 <body>
 <div class="container">
 <h2>Cadastro de Professor</h2>
 <form action="cadastro_professor.php" method="post">
-<label for "nome_professor">Nome:</label>
+<label for "nome_professor">Nome * :</label>
 <input type="text" id="nome_professor" name="nome_prof
-essor">
-<label for "senha_professor">Senha:</label>
-<input type="password" id="senha" name="senha_professo
-r">
-<label for "turma_id"> ID da Turma * :</label>
-<input type="number" id="turma_id" name="turma_id" req
-uired>
+
+essor" required>
+
+<label for "turma_id"> ID da Turma :</label>
+<input type="number" id="turma_id" name="turma_id">
+<h3>Disciplina:</h3>
+<select name="disciplina">
+<option value="Matematica">Matemática</option>
+<option value = "Portugues">Português</option>
+<option value = "Ciencias"> Ciencias </option>
+<option value = "Geografia"> Geografia </option>
+<option value = "Historia"> Historia </option>
+<option value = "Ingles"> Ingles </option>
+</select>
 <button type="submit">Cadastrar</button>
+</form>
 </body>
 </html>
